ca0cb1a0f1f545b498d2e8142d45d11e56e55757
JimmyCauja <jimmycauja@gmail.com>
2023-10-26 00:39:06 -0500

    Se trabajo con el perlin noise

--- sketch.js
+++ sketch.js
@@ -1,38 +1,33 @@
-// var dspX = 0;
-// var dspY = 10000;
-var inc = 0.09;
-var start = 0;
+let noiseTime = 0;
+let noiseTam = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(10, 35, 70);
+  smooth(50);
 }
 
 function draw() {
-  background(100,150);
-  stroke(255);
-  noFill();
-  beginShape();
-  var dspX = start;
-  for (var x = 0; x < windowWidth; x++) {
-    stroke(255);
-    var y = noise(dspX) * height;
-    vertex(x, y);
 
-    dspX += inc;
-  }
-  endShape();
-  start += inc;
+  let posX1 = map(noise(noiseTime), 0, 1, 0, windowWidth);
+  let posY1 = map(noise(noiseTam), 0, 1, 0, windowHeight);
+  let posX2 = map(noise(noiseTime + 0.1), 0, 1, 0, windowWidth);
+  let posY2 = map(noise(noiseTam + 0.1), 0, 1, 0, windowHeight);
 
-  // nooLoop();
+  let tam = map(noise(noiseTime), 0, 1, 50, 5);
+  noiseTime += 0.004;
+  noiseTam += 0.011;
+
+  stroke(179, 70, 90, 60);
+  strokeWeight(2);
+  line(posX1, posY1, posX2, posY2);
+
+  let reflY1 = windowHeight - posY1;
+  let reflY2 = windowHeight - posY2;
+  let tamrefl = tam;
+
+  stroke(243, 159, 90, 30);
+  strokeWeight(2);
+  line(posX1, reflY1, posX2, reflY2, tamrefl);
+  
 }
-  //   var x = map(noise(dspX), 0, 1, 0, windowWidth);
-  //   var y = map(noise(dspY), 0, 1, 0, windowHeight);
-  //   var coli = color(50, random(53, 160), random(151, 250));
-
-  //   dspX += 0.01;
-  //   dspY += 0.01;
-
-  //   ellipse(x, y, 24, 24);
-  //   fill(coli)
-  // }
-
